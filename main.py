664f0d15605f18489fa7ad6e2cbddc4aeb93163d
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-05-03 12:14:43 +1200

    V1

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 
-#
+#working on  quiz
 name = input ("hello, what is your name?")
 print("Hello",name,"welcome to my quiz")
 print("My name is Tristram")
+
+#asking  question and giving options
 answer=input("first question: 1+1: \n a.2 \n a.3 ")
+
+
