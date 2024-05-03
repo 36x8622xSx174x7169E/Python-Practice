78686003911371ad8989969f4065ec1c8b1b1033
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-05-03 12:59:26 +1200

    V2

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-
-
 #working on  quiz
 name = input ("hello, what is your name?")
 print("Hello",name,"welcome to my quiz")
 print("My name is Tristram")
 
 #asking  question and giving options
-answer=input("first question: 1+1: \n a.2 \n a.3 ")
+answer= input ("first question: 1+1: \n a.2 \n a. 3 \n a.2.5 ")
+if answer == "2":
+    print("correct")
+    print("good job!")
+#if the question is wrong it will say so using the else statment, if the question is right it will do the same
 
+else:
+    print("Incorrect")
+    print("Try again")
+#this is another question
+answer= input ("first question: 1+1: \n a.2 \n a. 3 \n a.2.5 ")
+   
+
+#this tells the user that the quiz is over
+    #end quiz
+print("thank you for playing my quiz")
 
