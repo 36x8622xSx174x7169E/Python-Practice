79f14659cbac042e357a349c5060728d92bca929
Pears0nt <159852283+36x8622xSx174x7169E@users.noreply.github.com>
2024-05-23 12:30:15 +1200

    lets go robert

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+
 import random
 play = "yes"
 QUESTION_FORMAT ="{}\nA.{}\nB.{}\nC.{}\nD.{}\n"
 GOOD_COMMENTS=["Way To Go!","Keep It Up!","Fantastic!"]
 BAD_COMMENTS=["Keep Trying","Maybe Next Time","Dont Give Up"]
 QUESTIONS= ["First Question:What Is 1+1","Second Question:What Is 2x3","Third Question: 2x=7"]
-OPTIONS = [["2","3","2.5","1"]["3","7","6","5"]["3","2.5","3","7"]]
+OPTIONS = [["2","3","2.5","1"],["3","7","6","5"],["2.5","3.5","3","7"]]
 SHORT_OPTIONS=["a","b","c","d"]
-
+ANSWERS = [0,2,1]
 
     #working on  quiz
 
@@ -40,85 +41,24 @@ while play== "yes" or  play == "y":
 
 
 
-    print("score=",score)
-    print("First Question")
-    question_attempts=tries
-    while question_attempts >0:
-        question = "1+1"
-        a = "2"
-        b = "3"
-        c = "2.5"
-        d = "1"
-        answer= input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
-        if answer == a or  answer == "a":
-            score+=10
-            print ("score=",score)
-            print("Correct")
-            print(GOOD_COMMENTS[1])
-            break
-        elif answer == "":
-            print("What you didn't even try!")
-            # != mean that it dose not equal
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("Thants not even an option!")
-            print("At least try!?")
-           
-        #if the question is wrong it will say so using the else statment, if the question is right it will answer using an if statement if they just hit enter
-        #the question will answere with an elif statement
-        else:
-            question_attempts -=1
-            print("Incorrect")
-            print(BAD_COMMENTS[0])
-        
-    #indent to make sure the code knows that it had a following result and that the the code is instantly finished
-    #this is another question
-    question_attempts=tries
-    while question_attempts >0:
-        question ="2x3"
-        a = "3"
-        b = "7"
-        c = "6"
-        d = "5"
-        answer= input (QUESTION_FORMAT.format(question, a, b ,c ,d)).lower()
+    for i in range(len(QUESTIONS)):
+        question_attempts=tries
+        while question_attempts >0:
+            answer=input(QUESTION_FORMAT.format(QUESTIONS[i],OPTIONS [i][0],OPTIONS [i][1],OPTIONS [i][2],OPTIONS [i][3])).lower()
+            if answer == (OPTIONS[i][ANSWERS[i]]) or answer == (SHORT_OPTIONS[ANSWERS[i]]):
+                print(random.choice(GOOD_COMMENTS))
+                score +=10
+                print("score =",score)
+                break
+            elif answer in SHORT_OPTIONS or answer in OPTIONS[i]:
+                print("Incorrect!")
+                print(random.choice(BAD_COMMENTS))
+                question_attempts -=1
+            else:
+                print("Thats Not Even An Option!")
+    
 
-        if answer =="6" or answer == "c":
-            score+=10
-            print("Correct")
-            print(random.choice (GOOD_COMMENTS))
-            break
-        elif answer == "":
-            print("What you didn't even try!")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("Thants not even an option!")
-            print("At least Try")
-        else:
-            question_attempts -=1
-            print("Incorrect")
-            print(BAD_COMMENTS[3])
-            
-    question_attempts=tries
-    while question_attempts >0:
-        question = "2x=7"
-        a = "3"
-        b = "2.5"
-        c = "2"
-        d = "7"
-        answer = input (QUESTION_FORMAT.format(question, a, b, c, d)).lower()
-        if answer =="2.5" or answer == "b".lower():
-            score+=10
-            print("Correct") 
-            print(GOOD_COMMENTS[3])
-            break
-        elif answer == "":
-            print("What you didn't even try!")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("Thats not even an option!")
-            print("At least try")
-        else:
-            question_attempts -=1
-            print("Incorrect")
-            print(BAD_COMMENTS[2])
-           
+        
 
 
 
